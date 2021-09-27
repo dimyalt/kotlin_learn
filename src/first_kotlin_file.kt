6983d2eb632e88124669f504a324c5d278f98367
dimyalt <79787789167@yandex.ru>
2021-09-27 13:34:37 +0300

    test commit

--- src/first_kotlin_file.kt
+++ src/first_kotlin_file.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    var first_name = readLine()
-    var second_name = readLine()
+    val first_name = readLine()
+    val second_name = readLine()
     println("${first_name}, ${second_name}, blya")
 
 
